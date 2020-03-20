f2dc60d0154f4be9264fb168964c4c3b0bcd4005
novariodaniel <novariodaniel@gmail.com>
2020-03-20 07:19:52 +0700

    create icon message in menu bar

--- application/views/admin/menu.php
+++ application/views/admin/menu.php
@@ -54,6 +54,13 @@
                     </li>
                 </ul>
 
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span> <span class="glyphicon glyphicon-envelope" style="font-size:18px;"></span></a>
+                        <ul class="dropdown-menu" id="dropdown-notif"></ul>
+                    </li>
+                </ul>
+
 
             </div>
             <!-- /.navbar-collapse -->
